4ccb5945010717333131379956a2d0d5686eb887
TimarTimar <tamasbelinszky@gmail.com>
2020-05-31 14:02:29 +0200

    citadel 1.0.0

--- technical-training-13.0-01-models/openacademy/models/citadel.py
+++ technical-training-13.0-01-models/openacademy/models/citadel.py
@@ -1,37 +1,38 @@
-from odoo import models,api, fields
+from odoo import models, api, fields
+
 
 class Classes(models.Model):
-    _name='classes.classes'
+    _name = 'classes.classes'
 
-    name=fields.Char(string="name")
-    level=fields.Integer(string="level")
-    is_active=fields.Boolean(string="active")
+    name = fields.Char(string="name")
+    level = fields.Integer(string="level")
+    is_active = fields.Boolean(string="active")
+
 
 class Maesters(models.Model):
-    _name='maesters.maesters'
+    _name = 'maesters.maesters'
 
-    name=fields.Char(string="name")
-    #m_session_id = fields.Many2One(sessions.sessions)
+    name = fields.Char(string="name")
+    # m_session_id = fields.Many2One(sessions.sessions)
 
 
 class Students(models.Model):
-    _name='students.students'
-
-    name=fields.Char(string="name")
-
-    session_id=fields.Many2one('sessions.sessions')
-    #related_field=fields.Char()
-
-class Sessions(models.Model):
-    _name='sessions.sessions'
+    _name = 'students.students'
 
     name = fields.Char(string="name")
 
+    session_id = fields.Many2one('sessions.sessions')
+    # related_field=fields.Char()
 
 
+class Sessions(models.Model):
+    _name = 'sessions.sessions'
+    # TODO add selection field
+    student_ids = fields.Many2many('students.students', string="Participants")
+    maesters_id = fields.Many2one('maesters.maesters', string="Teacher")
+    classes_id = fields.Many2one('classes.classes', ondelete='cascade', string="Class")
 
+    # students_session_ids=fields.One2Many(students.students, session_id, string="Students/Participants")
+    # participants=fields.One2Many(students.students, students.students.related_field)
 
-    #students_session_ids=fields.One2Many(students.students, session_id, string="Students/Participants")
-    #participants=fields.One2Many(students.students, students.students.related_field)
-    #TODO add selection field instead one2many
-    #maesters_session_ids = fields.One2Many(maesters.maesters, m_session_id, string="Maesters/Teachers")
+    # maesters_session_ids = fields.One2Many(maesters.maesters, m_session_id, string="Maesters/Teachers")
